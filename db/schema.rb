d3051c1a92dc9448e02c005a6a5212fa1aeaedcf
Paraprax <parkerjohnston28@gmail.com>
2017-04-17 20:26:39 -0400

    running migration to remove other info from new_locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418001338) do
+ActiveRecord::Schema.define(version: 20170418002329) do
 
   create_table "new_locations", force: :cascade do |t|
-    t.string  "branch_name"
-    t.string  "city_name"
-    t.string  "province_name"
-    t.integer "number_of_employees"
-    t.string  "weather"
+    t.string "city_name"
+    t.string "weather"
   end
 
   create_table "parts", force: :cascade do |t|
